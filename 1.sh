824951ee0e046176a34707584dcab221de14cb24
tusui-7 <tusui@hotgpt.dpdns.org>
2025-11-06 20:03:10 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,15 +1,4 @@
-
-mkdir -p "$PWD/bin/acme/ssl"
-cd "$PWD/bin/acme"
-if [ ! -f acme.sh ];then
-curl -sSL -o  acme.tar.gz https://github.com/acmesh-official/acme.sh/archive/master.tar.gz
-tar zxvf acme.tar.gz
-mv acme.sh-master/*  .
-rm -rf  acme.sh-master
-rm acme.tar.gz
-fi
-
-# ./acme.sh --install --nocron --home "$PWD/bin/acme/ssl"  -s  "email=XX@email.com"
-./acme.sh --set-default-ca --issue --server letsencrypt --home "./ssl" -d "$DYNV6_DNS" --dns dns_dynv6  --debug  --force
-
+PUBLIC_IP=$(curl --silent http://4.ipw.cn)
+curl "https://ipv4.dynv6.com/api/update?zone=$DYNV6_DNS&ipv4=8.8.8.8&token=$DYNV6_TOKEN"
+curl "https://ipv4.dynv6.com/api/update?zone=$DYNV6_DNS&ipv4=$PUBLIC_IP&token=$DYNV6_TOKEN"
 
