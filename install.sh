5d136843adbf000f4540faadde8824f1fe784783
tusui-7 <tusui@hotgpt.dpdns.org>
2025-11-06 14:44:43 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+
+
+
 #!/usr/bin/env sh
 
 PORT="${PORT:-8000}"
@@ -6,6 +9,7 @@ curl -sSL -o index.js https://raw.githubusercontent.com/tusui-7/hosting/refs/hea
 curl -sSL -o package.json https://raw.githubusercontent.com/tusui-7/hosting/refs/heads/main/package.json
 
 
+mkdir -p /home/container/file/music
 mkdir -p /home/container/bin/navidrome
 cd /home/container/bin/navidrome
 curl -sSL -o navidrome.tar.gz  https://github.com/navidrome/navidrome/releases/download/v0.58.0/navidrome_0.58.0_linux_amd64.tar.gz
