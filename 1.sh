81fd2df403a21486ee3c18dfa96cd321490cdaf8
tusui-7 <tusui@hotgpt.dpdns.org>
2025-11-06 22:46:27 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,6 +1,10 @@
 # nginx
-mkdir -p "$PWD/bin/nginx"
-cd "$PWD/bin/nginx"
+
+LOCAL_PATH="$PWD"
+
+
+mkdir -p "$LOCAL_PATH/bin/nginx"
+cd "$LOCAL_PATH/bin/nginx"
 if [ ! -f "./conf/nginx.conf" ];then
 curl -sSL -o  nginx.zip https://raw.githubusercontent.com/tusui-7/hosting/refs/heads/main/nginx.zip
 unzip  nginx.zip
@@ -12,7 +16,7 @@ cat > "./conf/nginx.conf" <<EOF
 user  nobody;
 worker_processes  2;
 
-error_log  $PWD/bin/nginx/log/error.log notice;
+error_log  $LOCAL_PATH/bin/nginx/log/error.log notice;
 pid        /var/run/nginx.pid;
 
 
@@ -22,14 +26,14 @@ events {
 
 
 http {
-    include       "$PWD/bin/nginx/conf/mime.types";
+    include       "$LOCAL_PATH/bin/nginx/conf/mime.types";
     default_type  application/octet-stream;
 
     log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
                       '$status $body_bytes_sent "$http_referer" '
                       '"$http_user_agent" "$http_x_forwarded_for"';
 
-    access_log  $PWD/bin/nginx/log/access.log  main;
+    access_log  $LOCAL_PATH/bin/nginx/log/access.log  main;
 
     sendfile        on;
     client_max_body_size 1024M;
@@ -40,10 +44,10 @@ http {
         listen       $PORT ssl ;
         server_name  $DYNV6_DNS;
 
-        root         "$PWD/bin/nginx/html";
+        root         "$LOCAL_PATH/bin/nginx/html";
 
-        ssl_certificate     "$PWD/bin/nginx/conf/ssl/fullchain.cer";
-        ssl_certificate_key "$PWD/bin/nginx/conf/ssl/$DYNV6_DNS.key";
+        ssl_certificate     "$LOCAL_PATH/bin/nginx/conf/ssl/fullchain.cer";
+        ssl_certificate_key "$LOCAL_PATH/bin/nginx/conf/ssl/$DYNV6_DNS.key";
         ssl_session_cache shared:SSL:1m;
         ssl_session_timeout  10m;
         ssl_ciphers HIGH:!aNULL:!MD5;
@@ -72,8 +76,8 @@ chmod +x "./nginx/nginx"
 
 
 ECC="_ecc"
-mkdir -p "$PWD/bin/nginx/conf/ssl"
-cp -ar "$PWD/bin/acme/ssl/$DYNV6_DNS$ECC/*" "$PWD/bin/nginx/conf/ssl"
+mkdir -p "$LOCAL_PATH/bin/nginx/conf/ssl"
+cp -ar "$LOCAL_PATH/bin/acme/ssl/$DYNV6_DNS$ECC/*" "$LOCAL_PATH/bin/nginx/conf/ssl"
 
 
 echo "ssl is ok"
