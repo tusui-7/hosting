4dd99844adab03965507da57228dae84b014f018
tusui-7 <tusui@hotgpt.dpdns.org>
2025-11-06 19:40:30 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -4,8 +4,8 @@ export DYNV6_DNS="musics.v6.navy"
 
 
 
-mkdir -p "$PWD/bin/acme"
 
+mkdir -p "$PWD/bin/acme/ssl"
 cd "$PWD/bin/acme"
 curl -sSL -o  acme.tar.gz https://github.com/acmesh-official/acme.sh/archive/master.tar.gz
 tar zxvf acme.tar.gz
@@ -13,9 +13,8 @@ mv acme.sh-master/*  .
 rm -rf  acme.sh-master
 rm acme.tar.gz
 
-./acme.sh --install --nocron --home "$PWD/bin/acme/ssl" --accountemial "XX@email.com"
+./acme.sh --install --nocron --home "$PWD/bin/acme/ssl" -s  "email=XX@email.com"
 
 ./acme.sh --issue --server letsencrypt --home . -d "$DYNV6_DNS" --dns dns_dynv6
-
 
 
